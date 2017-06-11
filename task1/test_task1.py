244f07a53edf2579e7ab67263bd2beb6390a2c5f
Roman Kachur <roman.kachur@globallogic.com>
2017-06-11 16:50:04 +0300

    Task #1. Added driver capabilities, added simple print of cookies.

--- task1/test_task1.py
+++ task1/test_task1.py
@@ -5,14 +5,23 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
 
-def test_google():
+def test_google(an_url = "https://google.com/"):
     #a_driver = webdriver.Chrome()
-    a_driver = webdriver.Chrome(ChromeDriverManager().install())
-    an_url = "https://google.com/"
+    options = webdriver.ChromeOptions()
+    options.add_argument("start-fullscreen")
+    #a_driver = webdriver.Chrome(ChromeDriverManager().install(),
+    #                           desired_capabilities={"chromeOptions": {"args": ["--start-fullscreen"]}})
+    a_driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options = options)
+
     a_driver.get(an_url)
     a_driver.find_element_by_name('q').send_keys('webdriver')
     a_driver.find_element_by_name('btnG').click()
     WebDriverWait(a_driver, 3).until(ec.title_is("webdriver - Пошук Google"))
+
+    cookies = a_driver.get_cookies()
+    print(cookies)
+    a_driver.delete_all_cookies()
+
     time.sleep(3)
     a_driver.quit()
 
