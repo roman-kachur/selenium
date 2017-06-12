3b8076e3d42ff883ec7050f6ed562e9729858731
roman-kachur <roman_kachur@ukr.net>
2017-06-12 16:28:27 +0200

    Task 2 completed.

---
+++ task2/test_task2.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
+
+def test_litecart(an_url = "http://localhost/litecart/admin/"):
+
+    username = 'admin'
+    password = 'admin'
+
+    # Initialize a driver:
+    options = webdriver.ChromeOptions()
+    options.add_argument("start-fullscreen")
+    a_driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options = options)
+
+    a_driver.get(an_url)
+    # Wait for a header:
+    WebDriverWait(a_driver, 3).until(ec.title_is("My Store"))
+    time.sleep(3)
+
+    # Enter username:
+    WebDriverWait(a_driver, 3).until(ec.presence_of_element_located((By.XPATH,'//*[@id="box-login"]/form/div[1]/div[1]/div/input')))
+    a_driver.find_element_by_xpath('//*[@id="box-login"]/form/div[1]/div[1]/div/input').send_keys(username)
+    time.sleep(1)
+
+    # Enter password:
+    WebDriverWait(a_driver, 3).until(ec.presence_of_element_located((By.XPATH,'//*[@id="box-login"]/form/div[1]/div[2]/div/input')))
+    a_driver.find_element_by_xpath('//*[@id="box-login"]/form/div[1]/div[2]/div/input').send_keys(password)
+    time.sleep(1)
+
+    # Login:
+    WebDriverWait(a_driver, 3).until(ec.presence_of_element_located((By.XPATH, '//*[@id="box-login"]/form/div[2]/button')))
+    a_driver.find_element_by_xpath('//*[@id="box-login"]/form/div[2]/button').click()
+
+    # Wait for a header:
+    WebDriverWait(a_driver, 3).until(ec.title_is("My Store"))
+    time.sleep(3)
+
+    # Logout:
+    WebDriverWait(a_driver, 3).until(ec.presence_of_element_located((By.XPATH, '// *[ @ id = "shortcuts"] / a[5] / i')))
+    a_driver.find_element_by_xpath('// *[ @ id = "shortcuts"] / a[5] / i').click()
+
+    time.sleep(3)
+    a_driver.quit()
+
+#test_litecart()
