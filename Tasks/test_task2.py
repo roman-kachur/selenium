b91feb0b205c71d2acbe8c1d183b0085d0d96cc7
Roman Kachur <roman.kachur@globallogic.com>
2017-06-20 21:20:41 +0300

    Task #2. Removed all explicit Waits. Only Implicit Wait is kept. Replaced all xpath into css_selector.

--- Tasks/test_task2.py
+++ Tasks/test_task2.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-import time
+from webdriver_manager.microsoft import IEDriverManager
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
+import time
 
 def test_litecart(an_url = "http://localhost/litecart/admin/"):
 
@@ -11,38 +12,35 @@ def test_litecart(an_url = "http://localhost/litecart/admin/"):
     password = 'admin'
 
     # Initialize a driver:
-    options = webdriver.ChromeOptions()
-    options.add_argument("start-fullscreen")
-    a_driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options = options)
+    #a_driver = webdriver.Ie(IEDriverManager().install())
+    #a_driver = webdriver.Chrome(ChromeDriverManager().install())
+    a_driver = webdriver.Firefox()
+    a_driver.implicitly_wait(3)
+
 
     a_driver.get(an_url)
     # Wait for a header:
     WebDriverWait(a_driver, 3).until(ec.title_is("My Store"))
-    time.sleep(3)
 
     # Enter username:
-    WebDriverWait(a_driver, 3).until(ec.presence_of_element_located((By.XPATH,'//*[@id="box-login"]/form/div[1]/div[1]/div/input')))
-    a_driver.find_element_by_xpath('//*[@id="box-login"]/form/div[1]/div[1]/div/input').send_keys(username)
-    time.sleep(1)
+    username_css_locator = 'input[name=username]'
+    a_driver.find_element_by_css_selector(username_css_locator).send_keys(username)
 
     # Enter password:
-    WebDriverWait(a_driver, 3).until(ec.presence_of_element_located((By.XPATH,'//*[@id="box-login"]/form/div[1]/div[2]/div/input')))
-    a_driver.find_element_by_xpath('//*[@id="box-login"]/form/div[1]/div[2]/div/input').send_keys(password)
-    time.sleep(1)
+    password_css_locator = 'input[name=password]'
+    a_driver.find_element_by_css_selector(password_css_locator).send_keys(password)
 
     # Login:
-    WebDriverWait(a_driver, 3).until(ec.presence_of_element_located((By.XPATH, '//*[@id="box-login"]/form/div[2]/button')))
-    a_driver.find_element_by_xpath('//*[@id="box-login"]/form/div[2]/button').click()
+    login_css_selector = 'button[name=login]'
+    a_driver.find_element_by_css_selector(login_css_selector).click()
 
     # Wait for a header:
     WebDriverWait(a_driver, 3).until(ec.title_is("My Store"))
-    time.sleep(3)
+    #time.sleep(3)
 
     # Logout:
-    WebDriverWait(a_driver, 3).until(ec.presence_of_element_located((By.XPATH, '// *[ @ id = "shortcuts"] / a[5] / i')))
-    a_driver.find_element_by_xpath('// *[ @ id = "shortcuts"] / a[5] / i').click()
+    logout_css_locator = 'i[class="fa fa-sign-out fa-lg"]'
+    a_driver.find_element_by_css_selector(logout_css_locator).click()
 
-    time.sleep(3)
+    time.sleep(2)
     a_driver.quit()
-
-#test_litecart()
