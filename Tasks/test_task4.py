ad646c876b3ec55a73bb8b8b663353553603c7c4
Roman Kachur <roman.kachur@globallogic.com>
2017-06-20 23:08:29 +0300

    Task #4. Replaced Explicit Wait by Implicit Wait. Menu walk through - remains on xpath.

--- Tasks/test_task4.py
+++ Tasks/test_task4.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.microsoft import IEDriverManager
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
@@ -8,50 +9,45 @@ import time
 
 def test_sidemenu():
 
-    # Initialize a driver:
-    options = webdriver.ChromeOptions()
-    options.add_argument("start-fullscreen")
-    a_driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
-
-    # Open url
-    an_url = "http://localhost/litecart/admin/"
     username = 'admin'
     password = 'admin'
+    an_url = "http://localhost/litecart/admin/"
+
+    # Initialize a driver:
+    #a_driver = webdriver.Ie(IEDriverManager().install())
+    a_driver = webdriver.Chrome(ChromeDriverManager().install())
+    #a_driver = webdriver.Firefox()
+    a_driver.implicitly_wait(3)
+
     a_driver.get(an_url)
+    # Wait for a header:
     WebDriverWait(a_driver, 3).until(ec.title_is("My Store"))
-    #time.sleep(1)
 
     # Enter username:
-    WebDriverWait(a_driver, 3).until(
-        ec.presence_of_element_located((By.XPATH, '//*[@id="box-login"]/form/div[1]/div[1]/div/input')))
-    a_driver.find_element_by_xpath('//*[@id="box-login"]/form/div[1]/div[1]/div/input').send_keys(username)
-    #time.sleep(1)
+    username_css_locator = 'input[name=username]'
+    a_driver.find_element_by_css_selector(username_css_locator).send_keys(username)
 
     # Enter password:
-    WebDriverWait(a_driver, 3).until(
-        ec.presence_of_element_located((By.XPATH, '//*[@id="box-login"]/form/div[1]/div[2]/div/input')))
-    a_driver.find_element_by_xpath('//*[@id="box-login"]/form/div[1]/div[2]/div/input').send_keys(password)
-    #time.sleep(1)
+    password_css_locator = 'input[name=password]'
+    a_driver.find_element_by_css_selector(password_css_locator).send_keys(password)
 
     # Login:
-    WebDriverWait(a_driver, 3).until(
-        ec.presence_of_element_located((By.XPATH, '//*[@id="box-login"]/form/div[2]/button')))
-    a_driver.find_element_by_xpath('//*[@id="box-login"]/form/div[2]/button').click()
+    login_css_selector = 'button[name=login]'
+    a_driver.find_element_by_css_selector(login_css_selector).click()
+
+    # Wait for a header:
     WebDriverWait(a_driver, 3).until(ec.title_is("My Store"))
-    #time.sleep(3)
+    time.sleep(3)
 
     # Walk through menu:
-    WebDriverWait(a_driver, 3).until(ec.presence_of_element_located((By.XPATH, '//*[@id="app-"]')))
     menu = a_driver.find_elements_by_xpath('//*[@id="app-"]')
 
     for index in range(1, len(menu) + 1):
         menu_item_x_path = '//*[@id="box-apps-menu"]/li[' + str(index) + ']'
-        WebDriverWait(a_driver, 3).until(ec.presence_of_element_located((By.XPATH, menu_item_x_path)))
         print(menu_item_x_path)
         a_driver.find_element_by_xpath(menu_item_x_path).click()
-        assert (WebDriverWait(a_driver, 3).until(
-            ec.presence_of_element_located((By.XPATH, '//*[@id="main"]/h1')))), "Element h1 not found"
-        #time.sleep(2)
+        assert (WebDriverWait(a_driver, 3).until
+                (ec.visibility_of_element_located((By.XPATH, '//*[@id="main"]/h1')))), "Element h1 not found"
 
         # Check for submenu:
         sub_menu = a_driver.find_elements_by_xpath(menu_item_x_path + '/ul/li')
@@ -59,20 +55,13 @@ def test_sidemenu():
             for jndex in range(1, len(sub_menu) + 1):
                 sub_menu_item_x_path = menu_item_x_path + '/ul/li[' + str(jndex) + ']'
                 print(sub_menu_item_x_path)
-
-                # Make a test on fallacy:
-                #if sub_menu_item_x_path == '//*[@id="box-apps-menu"]/li[8]/ul/li[5]':
-                #    assert (WebDriverWait(a_driver, 3).until(
-                #        ec.presence_of_element_located((By.XPATH, '//*[@id="main"]/abracadabda')))), "Element abracadabra not found"
-
-                WebDriverWait(a_driver, 3).until(ec.presence_of_element_located((By.XPATH, sub_menu_item_x_path)))
                 a_driver.find_element_by_xpath(sub_menu_item_x_path).click()
-                assert (WebDriverWait(a_driver, 3).until(
-                    ec.presence_of_element_located((By.XPATH, '//*[@id="main"]/h1')))), "Element h1 not found"
-                #time.sleep(2)
+                assert (WebDriverWait(a_driver, 3).until
+                        (ec.visibility_of_element_located((By.XPATH, '//*[@id="main"]/h1')))), "Element h1 not found"
 
     # Logout:
-    WebDriverWait(a_driver, 3).until(ec.presence_of_element_located((By.XPATH, '// *[ @ id = "shortcuts"] / a[5] / i')))
-    a_driver.find_element_by_xpath('// *[ @ id = "shortcuts"] / a[5] / i').click()
+    logout_css_locator = 'i[class="fa fa-sign-out fa-lg"]'
+    a_driver.find_element_by_css_selector(logout_css_locator).click()
+
     time.sleep(3)
     a_driver.quit()
