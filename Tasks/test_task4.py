5c9a13817ea9d99d5bf636bd8742574629b52e36
Roman Kachur <roman.kachur@globallogic.com>
2017-06-21 00:41:49 +0300

    Task #4. Added both menu walk: by xpath and by css. So far only Chrome and FF succeed. IE falls very often.

--- Tasks/test_task4.py
+++ Tasks/test_task4.py
@@ -14,10 +14,10 @@ def test_sidemenu():
     an_url = "http://localhost/litecart/admin/"
 
     # Initialize a driver:
-    a_driver = webdriver.Ie(IEDriverManager().install())
-    #a_driver = webdriver.Chrome(ChromeDriverManager().install())
+    #a_driver = webdriver.Ie(IEDriverManager().install())
+    a_driver = webdriver.Chrome(ChromeDriverManager().install())
     #a_driver = webdriver.Firefox()
-    a_driver.implicitly_wait(5)
+    a_driver.implicitly_wait(3)
 
     a_driver.get(an_url)
     # Wait for a header:
@@ -39,6 +39,19 @@ def test_sidemenu():
     WebDriverWait(a_driver, 3).until(ec.title_is("My Store"))
     time.sleep(3)
 
+    # How do you want to walk through it: by xpath or css ?
+    xpath_walk(a_driver)
+    css_walk(a_driver)
+
+    # Logout:
+    logout_css_locator = 'i[class="fa fa-sign-out fa-lg"]'
+    a_driver.find_element_by_css_selector(logout_css_locator).click()
+
+    time.sleep(3)
+    a_driver.quit()
+
+
+def xpath_walk(a_driver):
     # Walk through menu:
     menu = a_driver.find_elements_by_xpath('//*[@id="app-"]')
 
@@ -59,9 +72,29 @@ def test_sidemenu():
                 assert (WebDriverWait(a_driver, 3).until
                         (ec.visibility_of_element_located((By.XPATH, '//*[@id="main"]/h1')))), "Element h1 not found"
 
-    # Logout:
-    logout_css_locator = 'i[class="fa fa-sign-out fa-lg"]'
-    a_driver.find_element_by_css_selector(logout_css_locator).click()
 
-    time.sleep(3)
-    a_driver.quit()
+def css_walk(a_driver):
+
+    # Walk through menu:
+    menu_css = 'li[id="app-"]'
+    #WebDriverWait(a_driver, driver_wait).until(ec.presence_of_element_located((By.CSS_SELECTOR, menu_css)))
+    menu = a_driver.find_elements_by_css_selector(menu_css)
+
+    for index in range(1, len(menu) + 1):
+        menu_item_css = menu_css + ':nth-child(' + str(index) + ')'
+        WebDriverWait(a_driver, 3).until(ec.element_to_be_clickable((By.CSS_SELECTOR, menu_item_css)))
+        print(menu_item_css)
+        a_driver.find_element_by_css_selector(menu_item_css).click()
+        assert (WebDriverWait(a_driver, 3).until(ec.visibility_of_element_located((By.XPATH, '//*[@id="main"]/h1')))), "Element h1 not found"
+
+        # Check for submenu:
+        sub_menu_css = 'li[id^="doc-"]'
+        #WebDriverWait(a_driver, 3).until(ec.presence_of_element_located((By.CSS_SELECTOR, sub_menu_css)))
+        sub_menu = a_driver.find_elements_by_css_selector(sub_menu_css)
+        if sub_menu:
+            for jndex in range(1, len(sub_menu) + 1):
+                sub_menu_item_css = sub_menu_css + ':nth-child(' + str(jndex) + ')'
+                WebDriverWait(a_driver, 3).until(ec.element_to_be_clickable((By.CSS_SELECTOR, sub_menu_item_css)))
+                print(sub_menu_item_css)
+                a_driver.find_element_by_css_selector(sub_menu_item_css).click()
+                assert (WebDriverWait(a_driver, 3).until(ec.visibility_of_element_located((By.XPATH, '//*[@id="main"]/h1')))), "Element h1 not found"
