65e5115d1cdd64731047ed4f76ef74870fd3642d
roman-kachur <roman_kachur@ukr.net>
2017-06-21 14:28:39 +0200

    Task#5. Moved from xpath to css. IE still fails with Explicit Waits.

--- Tasks/test_task5.py
+++ Tasks/test_task5.py
@@ -14,89 +14,91 @@ def test_product():
 
     #  Check it in Chrome:
     a_driver = webdriver.Chrome(ChromeDriverManager().install())
+    a_driver.implicitly_wait(3)
+    check_it(a_driver, an_url)
+
+    # Check it in Firefox:
+    #a_driver = webdriver.Firefox(GeckoDriverManager().install())
+    a_driver = webdriver.Firefox()
+    a_driver.implicitly_wait(3)
     check_it(a_driver, an_url)
 
     # Check it in IE:
     a_driver = webdriver.Ie(IEDriverManager().install())
+    a_driver.implicitly_wait(3)
     check_it(a_driver, an_url)
 
-    # Check it in Firefox:
-    a_driver = webdriver.Firefox(GeckoDriverManager().install())
-    a_driver = webdriver.Firefox()
-    check_it(a_driver, an_url)
+
 
 
 def check_it(a_driver, an_url):
     # Open url
     a_driver.get(an_url)
     WebDriverWait(a_driver, 3).until(ec.title_is("My Store | Online Store"))
-    #time.sleep(1)
 
     # Select Campaign Product tab:
-    camp_prod_tab_xpath = '//*[@id="content"]/ul/li[1]'
-    WebDriverWait(a_driver, 3).until(ec.presence_of_element_located((By.XPATH, camp_prod_tab_xpath)))
-    capmaign_product = a_driver.find_element_by_xpath(camp_prod_tab_xpath)
-    #time.sleep(3)
+    camp_prod_tab_css = 'a[href="#campaign-products"]'
+    #WebDriverWait(a_driver, 3).until(ec.presence_of_element_located((By.CSS_SELECTOR, camp_prod_tab_css)))
+    capmaign_product = a_driver.find_element_by_css_selector(camp_prod_tab_css)
+    capmaign_product.click()
+    time.sleep(2)
 
     # Select first product:
-    first_product_xpath = '//*[@id="box-campaign-products"]/div/div/div/a[1]'
-    WebDriverWait(a_driver, 3).until(ec.presence_of_element_located((By.XPATH, first_product_xpath)))
-    first_product = a_driver.find_element_by_xpath(first_product_xpath)
+    products_css = 'div[id="box-campaign-products"]'
+    first_product_css = products_css + ' div[class^="col-xs"]:nth-child(1)'
+    #WebDriverWait(a_driver, 3).until(ec.presence_of_all_elements_located((By.CSS_SELECTOR, first_product_css)))
+    first_product = a_driver.find_element_by_css_selector(first_product_css)
 
     # Get first item's properties on Main page:
     # Name:
-    first_product_name_xpath = '//*[@id="box-campaign-products"]/div/div/div/a[1]/div[2]/div[1]'
-    first_product_name = a_driver.find_element_by_xpath(first_product_name_xpath)
+    first_product_name_css = first_product_css + ' div[class="name"]'
+    #WebDriverWait(a_driver, 3).until(ec.presence_of_all_elements_located((By.CSS_SELECTOR, first_product_name_css)))
+    first_product_name = a_driver.find_element_by_css_selector(first_product_name_css)
     a_name = first_product_name.get_attribute('textContent')
 
     # Regular Price value and style:
-    first_product_regprice_xpath = '//*[@id="box-campaign-products"]/div/div/div/a[1]/div[2]/div[3]/s'
-    first_product_regprice = a_driver.find_element_by_xpath(first_product_regprice_xpath)
+    first_product_regprice_css = first_product_css + ' s[class ="regular-price"]'
+    #WebDriverWait(a_driver, 3).until(ec.presence_of_element_located((By.CSS_SELECTOR, first_product_regprice_css)))
+    first_product_regprice = a_driver.find_element_by_css_selector(first_product_regprice_css)
     a_regvalue = first_product_regprice.get_attribute('textContent')
     a_regcolor = first_product_regprice.value_of_css_property("color")
     a_regstyle = first_product_regprice.value_of_css_property("text-decoration-line")
 
     # Campaign Price value and style:
-    first_product_campprice_xpath = '//*[@id="box-campaign-products"]/div/div/div/a[1]/div[2]/div[3]/strong'
-    first_product_campprice = a_driver.find_element_by_xpath(first_product_campprice_xpath)
+    first_product_campprice_css = first_product_css + ' strong[class ="campaign-price"]'
+    #WebDriverWait(a_driver, 3).until(ec.presence_of_element_located((By.CSS_SELECTOR, first_product_campprice_css)))
+    first_product_campprice = a_driver.find_element_by_css_selector(first_product_campprice_css)
     a_campvalue = first_product_campprice.get_attribute('textContent')
     a_campcolor = first_product_campprice.value_of_css_property("color")
     a_campstyle = first_product_campprice.value_of_css_property("font-weight")
 
-
-    # Click on selected item:
+    # Finally, click on the first product:
     first_product.click()
-    clicked_product_xpath = '//*[@id="box-product"]'
-    WebDriverWait(a_driver, 3).until(ec.visibility_of_element_located((By.XPATH, clicked_product_xpath)))
-    #time.sleep(3)
+    clicked_product_css = 'div[id="box-product"]'
+    WebDriverWait(a_driver, 3).until(ec.visibility_of_element_located((By.CSS_SELECTOR, clicked_product_css)))
 
-    # Get ckicked item's properties:
+    # Get clicked item's properties:
     # Name:
-    clicked_item_name_xpath = '//*[@id="box-product"]/div[1]/div[2]/h1'
-    clicked_item_name = a_driver.find_element_by_xpath(clicked_item_name_xpath)
+    clicked_item_name_css = clicked_product_css + ' h1[class="title"]'
+    #WebDriverWait(a_driver, 3).until(ec.presence_of_element_located((By.CSS_SELECTOR, clicked_item_name_css)))
+    clicked_item_name = a_driver.find_element_by_css_selector(clicked_item_name_css)
     clicked_name = clicked_item_name.get_attribute('textContent')
 
     # Clicked Regular Price value and style:
-    clicked_product_regprice_xpath = '//*[@id="box-product"]/div[1]/div[3]/div/div[1]/del'
-    clicked_product_regprice = a_driver.find_element_by_xpath(clicked_product_regprice_xpath)
+    clicked_product_regprice_css = clicked_product_css + ' del[class ="regular-price"]'
+    #WebDriverWait(a_driver, 3).until(ec.presence_of_element_located((By.CSS_SELECTOR, clicked_product_regprice_css)))
+    clicked_product_regprice = a_driver.find_element_by_css_selector(clicked_product_regprice_css)
     clicked_regvalue = clicked_product_regprice.get_attribute('textContent')
     clicked_regcolor = clicked_product_regprice.value_of_css_property("color")
     clicked_regstyle = clicked_product_regprice.value_of_css_property("text-decoration-line")
 
     # Clicked Campaign Price value and style:
-    clicked_product_campprice_xpath = '//*[@id="box-product"]/div[1]/div[3]/div/div[1]/strong'
-    clicked_product_campprice = a_driver.find_element_by_xpath(clicked_product_campprice_xpath)
+    clicked_product_campprice_css = clicked_product_css + ' strong[class ="campaign-price"]'
+    #WebDriverWait(a_driver, 3).until(ec.presence_of_element_located((By.CSS_SELECTOR, clicked_product_campprice_css)))
+    clicked_product_campprice = a_driver.find_element_by_css_selector(clicked_product_campprice_css)
     clicked_campvalue = clicked_product_campprice.get_attribute('textContent')
     clicked_campcolor = clicked_product_campprice.value_of_css_property("color")
     clicked_campstyle = clicked_product_campprice.value_of_css_property("font-weight")
-
-
-    # Print out values before comparision:
-    print("First Items's attributes are: ", a_name, a_regvalue, a_regcolor, a_regstyle,
-          a_campvalue, a_campcolor, a_campstyle)
-    print("Clicked Item's attributes are: ", clicked_name, clicked_regvalue, clicked_regcolor, clicked_regstyle,
-          clicked_campvalue, clicked_campcolor, clicked_campstyle)
-
 
     # Compare values:
     assert (a_name == clicked_name), "Product Name doesn't match"
