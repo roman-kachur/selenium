5a05c2bd2cf2c10b030fe64f93b931927b781ea3
Roman Kachur <roman.kachur@globallogic.com>
2017-06-22 22:08:59 +0300

    Task #4. Completed. Works on Chrome/FF/IE.

--- Tasks/test_task4.py
+++ Tasks/test_task4.py
@@ -9,92 +9,73 @@ import time
 
 def test_sidemenu():
 
-    username = 'admin'
-    password = 'admin'
-    an_url = "http://localhost/litecart/admin/"
-
     # Initialize a driver:
     #a_driver = webdriver.Ie(IEDriverManager().install())
     a_driver = webdriver.Chrome(ChromeDriverManager().install())
     #a_driver = webdriver.Firefox()
-    a_driver.implicitly_wait(3)
 
-    a_driver.get(an_url)
-    # Wait for a header:
-    WebDriverWait(a_driver, 3).until(ec.title_is("My Store"))
+    # Walk through menus in separate function:
+    menu_walk(a_driver)
 
-    # Enter username:
-    username_css_locator = 'input[name=username]'
-    a_driver.find_element_by_css_selector(username_css_locator).send_keys(username)
-
-    # Enter password:
-    password_css_locator = 'input[name=password]'
-    a_driver.find_element_by_css_selector(password_css_locator).send_keys(password)
-
-    # Login:
-    login_css_selector = 'button[name=login]'
-    a_driver.find_element_by_css_selector(login_css_selector).click()
-
-    # Wait for a header:
-    WebDriverWait(a_driver, 3).until(ec.title_is("My Store"))
-    time.sleep(3)
-
-    # How do you want to walk through it: by xpath or css ?
-    xpath_walk(a_driver)
-    css_walk(a_driver)
-
-    # Logout:
-    logout_css_locator = 'i[class="fa fa-sign-out fa-lg"]'
-    a_driver.find_element_by_css_selector(logout_css_locator).click()
-
-    time.sleep(3)
+    # Close the driver:
     a_driver.quit()
 
 
-def xpath_walk(a_driver):
-    # Walk through menu:
-    menu = a_driver.find_elements_by_xpath('//*[@id="app-"]')
+def menu_walk(a_driver):
 
-    for index in range(1, len(menu) + 1):
-        menu_item_x_path = '//*[@id="box-apps-menu"]/li[' + str(index) + ']'
-        print(menu_item_x_path)
-        a_driver.find_element_by_xpath(menu_item_x_path).click()
-        assert (WebDriverWait(a_driver, 3).until
-                (ec.visibility_of_element_located((By.XPATH, '//*[@id="main"]/h1')))), "Element h1 not found"
+    # Definitions:
+    a_driver.implicitly_wait(1)
+    username = 'admin'
+    password = 'admin'
+    driver_wait = 3                     # 3 seconds for explicit waits.
+    page_header_css = 'h1'              # Expected condition for assertions.
+    time_sleep = 1                      # Intentional Wait for menu/submenu items to load completely.
+    an_url = "http://localhost/litecart/admin/"
 
-        # Check for submenu:
-        sub_menu = a_driver.find_elements_by_xpath(menu_item_x_path + '/ul/li')
-        if sub_menu:
-            for jndex in range(1, len(sub_menu) + 1):
-                sub_menu_item_x_path = menu_item_x_path + '/ul/li[' + str(jndex) + ']'
-                print(sub_menu_item_x_path)
-                a_driver.find_element_by_xpath(sub_menu_item_x_path).click()
-                assert (WebDriverWait(a_driver, 3).until
-                        (ec.visibility_of_element_located((By.XPATH, '//*[@id="main"]/h1')))), "Element h1 not found"
+    # Lets go. Open url:
+    a_driver.get(an_url)
+    # Wait for a header:
+    WebDriverWait(a_driver, 3).until(ec.title_is("My Store"))
+    # Enter username:
+    username_css_locator = 'input[name=username]'
+    a_driver.find_element_by_css_selector(username_css_locator).send_keys(username)
+    # Enter password:
+    password_css_locator = 'input[name=password]'
+    a_driver.find_element_by_css_selector(password_css_locator).send_keys(password)
+    # Login:
+    login_css_selector = 'button[name=login]'
+    a_driver.find_element_by_css_selector(login_css_selector).click()
+    # Wait for a header:
+    WebDriverWait(a_driver, 3).until(ec.title_is("My Store"))
 
-
-def css_walk(a_driver):
 
     # Walk through menu:
     menu_css = 'li[id="app-"]'
-    #WebDriverWait(a_driver, driver_wait).until(ec.presence_of_element_located((By.CSS_SELECTOR, menu_css)))
+    time.sleep(time_sleep)
     menu = a_driver.find_elements_by_css_selector(menu_css)
 
     for index in range(1, len(menu) + 1):
         menu_item_css = menu_css + ':nth-child(' + str(index) + ')'
-        WebDriverWait(a_driver, 3).until(ec.element_to_be_clickable((By.CSS_SELECTOR, menu_item_css)))
+        WebDriverWait(a_driver, driver_wait).until(ec.element_to_be_clickable((By.CSS_SELECTOR, menu_item_css)))
         print(menu_item_css)
         a_driver.find_element_by_css_selector(menu_item_css).click()
-        assert (WebDriverWait(a_driver, 3).until(ec.visibility_of_element_located((By.XPATH, '//*[@id="main"]/h1')))), "Element h1 not found"
+        assert (WebDriverWait(a_driver, driver_wait).until
+                (ec.visibility_of_element_located((By.CSS_SELECTOR, page_header_css)))), "Element h1 not found"
 
         # Check for submenu:
         sub_menu_css = 'li[id^="doc-"]'
-        #WebDriverWait(a_driver, 3).until(ec.presence_of_element_located((By.CSS_SELECTOR, sub_menu_css)))
+        time.sleep(time_sleep)
         sub_menu = a_driver.find_elements_by_css_selector(sub_menu_css)
         if sub_menu:
             for jndex in range(1, len(sub_menu) + 1):
                 sub_menu_item_css = sub_menu_css + ':nth-child(' + str(jndex) + ')'
-                WebDriverWait(a_driver, 3).until(ec.element_to_be_clickable((By.CSS_SELECTOR, sub_menu_item_css)))
+                WebDriverWait(a_driver, driver_wait).until(ec.element_to_be_clickable((By.CSS_SELECTOR, sub_menu_item_css)))
                 print(sub_menu_item_css)
                 a_driver.find_element_by_css_selector(sub_menu_item_css).click()
-                assert (WebDriverWait(a_driver, 3).until(ec.visibility_of_element_located((By.XPATH, '//*[@id="main"]/h1')))), "Element h1 not found"
+                assert (WebDriverWait(a_driver, 3).until
+                        (ec.visibility_of_element_located((By.CSS_SELECTOR, page_header_css)))), "Element h1 not found"
+
+    # Logout:
+    logout_css_locator = 'i[class="fa fa-sign-out fa-lg"]'
+    a_driver.find_element_by_css_selector(logout_css_locator).click()
+    time.sleep(1)
