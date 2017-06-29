21d1be8e7a070372d573336b565962b62221b8aa
roman-kachur <roman_kachur@ukr.net>
2017-06-29 11:33:42 +0300

    Task#7. Scenario - changed.

--- Tasks/test_task7.py
+++ Tasks/test_task7.py
@@ -15,10 +15,10 @@ def test_cart():
 
     # Initialize a driver:
     #a_driver = webdriver.Ie(IEDriverManager().install())
-    #a_driver = webdriver.Firefox()
-    a_driver = webdriver.Chrome(ChromeDriverManager().install())
-    a_driver.implicitly_wait(2)
-    wait = WebDriverWait(a_driver, 4)
+    a_driver = webdriver.Firefox()
+    #a_driver = webdriver.Chrome(ChromeDriverManager().install())
+    a_driver.implicitly_wait(1)
+    wait = WebDriverWait(a_driver, 2)
 
     a_driver.get(an_url)
     # Wait for a header:
@@ -31,46 +31,50 @@ def test_cart():
     capmaign_product = a_driver.find_element_by_css_selector(camp_prod_tab_css)
     capmaign_product.click()
 
-    # Select and click on the first product on the tab:
-    products_css = 'div[id="box-campaign-products"]'
-    first_product_css = products_css + ' div[class^="col-xs"]:nth-child(1)'
-    first_product = a_driver.find_element_by_css_selector(first_product_css)
-    first_product.click()
-
-    # Switch to full page view:
-    full_page_view_css = 'div[id="view-full-page"]'
-    full_page_view = a_driver.find_element_by_css_selector(full_page_view_css)
-    full_page_view.click()
-
-    # Choose item's Size=Small:
-    size_css = 'select[class ="form-control"] option[value="Small"]'
-    size = a_driver.find_element_by_css_selector(size_css)
-    size.click()
-
     # Check current items in the cart:
     cart_items_css = 'span[class="quantity"]'
     cart_items = a_driver.find_element_by_css_selector(cart_items_css)
 
-    # Button 'Add to cart':
-    add_button_css = 'button[name="add_cart_product"]'
-    add_button = a_driver.find_element_by_css_selector(add_button_css)
 
-    # Add it to the cart three times:
+    # Add the first product to the cart three times:
     for i in range(3):
         # Check current items in the cart:
         items = cart_items.text
 
-        # Click Add button:
+        # Select and click on the first product on the tab:
+        products_css = 'div[id="box-campaign-products"]'
+        first_product_css = products_css + ' div[class^="col-xs"]:nth-child(1)'
+        first_product = a_driver.find_element_by_css_selector(first_product_css)
+        first_product.click()
+
+        # Switch to full page view:
+        # full_page_view_css = 'div[id="view-full-page"]'
+        # full_page_view = a_driver.find_element_by_css_selector(full_page_view_css)
+        # full_page_view.click()
+
+        # If present, try to choose item's Size=Small:
+        size_css = 'select[class ="form-control"] option[value="Small"]'
+        try:
+            size_presence = wait.until(
+                ec.presence_of_element_located((By.CSS_SELECTOR, size_css)))
+        except:
+            size_presence = False
+        if size_presence:
+            a_driver.find_element_by_css_selector(size_css).click()
+
+        # Click Button 'Add to cart':
+        add_button_css = 'button[name="add_cart_product"]'
+        add_button = a_driver.find_element_by_css_selector(add_button_css)
         add_button.click()
+
+        # Close item's window:
+        close_button_css = 'button[aria-label="Close"]'
+        close_button = a_driver.find_element_by_css_selector(close_button_css)
+        close_button.click()
 
         # Wait until cart's items is updated:
         plus_one = str(int(items) + 1)
         wait.until(ec.text_to_be_present_in_element((By.CSS_SELECTOR, cart_items_css), plus_one))
-
-        ## Close item's window:
-        #close_button_css = 'button[aria-label="Close"]'
-        #close_button = a_driver.find_element_by_css_selector(close_button_css)
-        #close_button.click()
 
 
     # Navigate to the cart:
